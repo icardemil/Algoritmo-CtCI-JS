bd8141fad2d75eecea17b470f898a6e621e38fc0
icardemil64 <icardemil64@gmail.com>
2021-10-04 21:53:40 -0300

    urlify

---
+++ C-01/urlify.js
@@ -0,0 +1,23 @@
+//Cadenas con muchos espacios al final
+const urlify = (string) => {
+  str1 = string.trim().split("");
+  for (let idx in str1) {
+    if (str1[idx] === " ") {
+      str1[idx] = "%20";
+    }
+  }
+  return str1.join("");
+};
+
+//console.log(urlify("Hola mundo        "));
+//console.log(urlify("Mr John Smith"));
+
+//Cadenas con muchos espacios en todas partes 🐱‍👓
+const urlifyW = (string) => {
+  const reg = "s ";
+  const str2 = string.trim().replace(/\s+/g, "%20");
+  return str2;
+};
+
+console.log(urlifyW("  Hola   mundo        "));
+console.log(urlifyW("Mr John Smith"));
